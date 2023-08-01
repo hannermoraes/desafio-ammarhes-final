dbb97f85302488552c9d73df5eb7886fe72ede27
Hanner Moraes <101107412+hannermoraes@users.noreply.github.com>
2023-08-01 11:16:20 -0300

    correções e ajustes

--- js/script.js
+++ js/script.js
@@ -1,86 +1,142 @@
-const loginContainer = document.getElementById('login-container');
-const moveOverlay = () => loginContainer.classList.toggle('move');
+const getElement = (selector) => document.querySelector(selector);
 
-document.getElementById('open-register').addEventListener('click', moveOverlay);
-document.getElementById('open-login').addEventListener('click', moveOverlay);
-document.getElementById('open-register-mobile').addEventListener('click', moveOverlay);
-document.getElementById('open-login-mobile').addEventListener('click', moveOverlay);
+// Elementos do DOM
+const elements = {
+  loginContainer: getElement('#login-container'),
+  passwordInput: getElement('#password-input'),
+  outSenha: getElement('#outSenha'),
+  emailInput: getElement('#email'),
+  passwordInputLogin: getElement('#password'),
+  submitButton: getElement('#submit-button'),
+  fullNameInput: getElement('.form-register input[placeholder="Nome Completo"]'),
+  emailInputRegister: getElement('.form-register input[placeholder="Email"]'),
+  passwordInputRegister: getElement('.form-register input[placeholder="Senha"]'),
+  formLoginButton: getElement('.form-login .form-button'),
+  formRegisterButton: getElement('.form-register .form-button'),
+  openRegisterButton: getElement('#open-register'),
+  openLoginButton: getElement('#open-login'),
+  openRegisterButtonMobile: getElement('#open-register-mobile'),
+  openLoginButtonMobile: getElement('#open-login-mobile'),
+};
 
-const passwordInput = document.getElementById('password-input');
-const outSenha = document.getElementById('outSenha');
+// Toggle da classe 'move' no container de login
+const moveOverlay = () => elements.loginContainer.classList.toggle('move');
 
-passwordInput.addEventListener('click', mostrarRequisitosSenha);
-passwordInput.addEventListener('blur', limparRequisitosSenha);
 
-function mostrarRequisitosSenha() {
-  outSenha.textContent = "1 - A senha precisa ter no mínimo 8 caracteres.\n2 - É necessário ter ao menos uma letra maiúscula. (Ex: A-Z).\n3 - É necessário pelo menos um caractere especial. (Ex:@$&).\n4 - É preciso ter ao menos um número. (Ex: 1,2,3...).";
-}
+// Listener para o botão de cadastro mobile (abrir overlay)
+elements.openRegisterButtonMobile.addEventListener('click', () => {
+  moveOverlay();
+  clearRequirementsPassword(); // Limpar requisitos da senha quando o overlay for aberto
+});
 
-function limparRequisitosSenha() {
-  outSenha.textContent = "";
-}
+// Listener para o botão de entrar mobile (fechar overlay)
+elements.openLoginButtonMobile.addEventListener('click', () => {
+  moveOverlay();
+  clearRequirementsPassword(); // Limpar requisitos da senha quando o overlay for fechado
+});
 
-// Função para renderizar o reCAPTCHA
-function renderRecaptcha() {
-  grecaptcha.render('submit-button', {
-    'sitekey': '6LfZrGEnAAAAAGVFHbMjtY7oxqaU_lccv3TXQ_1V',
-    'callback': ""
-  });
-}
+// Mostrar requisitos da senha
+const showRequirementsPassword = () => {
+  elements.outSenha.textContent =
+    " 1 - No mínimo 8 caracteres.\n" +
+    "2 - Ao menos uma letra maiúscula. (Ex: A-Z).\n" +
+    "3 - Ao menos um caractere especial. (Ex: @$&).\n" +
+    "4 - Ao menos um número. (Ex: 1, 2, 3...)."
+};
 
-// Chamando a função para renderizar o reCAPTCHA quando a página carrega
-window.onload = renderRecaptcha;
+// Limpar requisitos da senha
+const clearRequirementsPassword = () => {
+  elements.outSenha.textContent = '';
+};
 
-function handleLogin() {
-  // Verificar se o reCAPTCHA foi resolvido
+// Função para verificar o reCAPTCHA
+const isRecaptchaResolved = () => {
   const recaptchaResponse = grecaptcha.getResponse();
   if (!recaptchaResponse) {
-    alert("Por favor, complete o reCAPTCHA antes de fazer o login.");
+    alert('Complete o reCAPTCHA antes de fazer o seu login.');
+    return false;
+  }
+  return true;
+};
+
+// Função para realizar o login
+const handleLogin = () => {
+  if (!isRecaptchaResolved()) {
     return;
   }
 
-  // Se o reCAPTCHA foi resolvido, pode fazer o login
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  const email = elements.emailInput.value;
+  const password = elements.passwordInputLogin.value;
 
   if (email === '' || password === '') {
     alert('Por favor, preencha todos os campos.');
     return;
   }
 
-  // Realizar a validação do login
   const loginValid = validateLogin(email, password);
   if (loginValid) {
-    // Exibir mensagem de login bem-sucedido (apenas para fins de demonstração)
-    alert("Login bem-sucedido!");
-    // Limpando o reCAPTCHA após o login bem-sucedido
+    alert('Login bem-sucedido!');
     grecaptcha.reset();
   } else {
-    // Usuário não encontrado
-    alert('Usuário não encontrado!\nVerifique se email e senha estão corretos.');
+    alert('Usuário não encontrado!\nVerifique se o email e a senha estão corretos.');
     grecaptcha.reset();
   }
-}
+};
 
-// Restante do código, incluindo as funções de validateLogin e saveUser (não foram alteradas)
+// Função para realizar o registro
+const handleRegister = () => {
+  const fullName = elements.fullNameInput.value;
+  const email = elements.emailInputRegister.value;
+  const password = elements.passwordInputRegister.value;
+
+  if (fullName === '' || email === '' || password === '') {
+    alert('Por favor, preencha todos os campos.');
+    return;
+  }
+
+  saveUser(email, password, fullName);
+};
+
+// Adicionar os listeners aos botões de registro e login
+elements.formLoginButton.addEventListener('click', handleLogin);
+elements.formRegisterButton.addEventListener('click', handleRegister);
+
+// Adicionar os listeners para mostrar e limpar os requisitos da senha
+elements.passwordInput.addEventListener('click', showRequirementsPassword);
+elements.passwordInput.addEventListener('blur', clearRequirementsPassword);
+elements.passwordInput.addEventListener('focus', showRequirementsPassword);
+
+// Listener para o botão de cadastro (abrir overlay)
+elements.openRegisterButton.addEventListener('click', () => {
+  moveOverlay();
+  clearRequirementsPassword(); // Limpar requisitos da senha quando o overlay for aberto
+});
+
+// Listener para o botão de entrar (fechar overlay)
+elements.openLoginButton.addEventListener('click', () => {
+  moveOverlay();
+  clearRequirementsPassword(); // Limpar requisitos da senha quando o overlay for fechado
+});
+
+// Função para renderizar o reCAPTCHA quando a página carrega
+window.onload = () => {
+  grecaptcha.render('submit-button', {
+    sitekey: '6LfZrGEnAAAAAGVFHbMjtY7oxqaU_lccv3TXQ_1V',
+    callback: '',
+  });
+};
+
+// Função para validar login e salvar usuário
 function validateLogin(email, password) {
   const registeredUsers = JSON.parse(localStorage.getItem('registeredUsers')) || [];
-  const user = registeredUsers.find(user => user.email === email);
+  const user = registeredUsers.find((user) => user.email === email);
 
-  if (!user) {
-    return false;
-  }
-
-  if (user.password === password) {
-    return true;
-  } else {
-    return false;
-  }
+  return user && user.password === password;
 }
 
 function saveUser(email, password, fullName) {
   const registeredUsers = JSON.parse(localStorage.getItem('registeredUsers')) || [];
-  const existingUser = registeredUsers.find(user => user.email === email);
+  const existingUser = registeredUsers.find((user) => user.email === email);
 
   if (existingUser) {
     alert('Este email já está registrado. Por favor, use um email diferente.');
@@ -89,7 +145,11 @@ function saveUser(email, password, fullName) {
 
   const passwordRegex = /^(?=.*\d)(?=.*[A-Z])(?=.*[.!@#$%^&*_\-])[A-Za-z\d!@#$%.^&*_\-]{8,}$/;
   if (!passwordRegex.test(password)) {
-    alert('1 - A senha precisa ter no mínimo 8 caracteres.\n2 - É necessário ter ao menos uma letra maiúscula. (Ex: A-Z).\n3 - É necessário pelo menos um caractere especial. (Ex:@$&).\n4 - É preciso ter ao menos um número. (Ex: 1,2,3...).');
+    alert(
+      " 1 - No mínimo 8 caracteres.\n" +
+      "2 - Ao menos uma letra maiúscula. (Ex: A-Z).\n" +
+      "3 - Ao menos um caractere especial. (Ex: @$&).\n" +
+      "4 - Ao menos um número. (Ex: 1, 2, 3...).");
     return;
   }
 
@@ -98,25 +158,8 @@ function saveUser(email, password, fullName) {
 
   alert('Conta criada com sucesso! Agora você pode fazer login com suas credenciais.');
 
-  document.querySelectorAll('.form-register .form-input').forEach(input => input.value = '');
+  // Limpar campos após o cadastro
+  elements.fullNameInput.value = '';
+  elements.emailInputRegister.value = '';
+  elements.passwordInputRegister.value = '';
 }
-
-function handleRegister() {
-  const fullName = document.querySelector('.form-register input[placeholder="Nome Completo"]').value;
-  const email = document.querySelector('.form-register input[placeholder="Email"]').value;
-  const password = document.querySelector('.form-register input[placeholder="Senha"]').value;
-
-  if (fullName === '' || email === '' || password === '') {
-    alert('Por favor, preencha todos os campos.');
-    return;
-  }
-
-  saveUser(email, password, fullName);
-}
-
-// Adicionar os listeners para os botões de registro e login (se existirem)
-document.querySelector('.form-login .form-button').addEventListener('click', handleLogin);
-document.querySelector('.form-register .form-button').addEventListener('click', handleRegister);
-
-
-
